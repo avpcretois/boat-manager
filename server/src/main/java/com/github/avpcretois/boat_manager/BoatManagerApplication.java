088e1c9d493a12ac5112d8aa733adebec1d4ff0a
Andréas Crétois <avpcretois@use.startmail.com>
2025-12-08 17:23:13 +0100

    Bootstraping the frameworks
    There's still configuration to do but both the front and the back build correctly

---
+++ server/src/main/java/com/github/avpcretois/boat_manager/BoatManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.avpcretois.boat_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatManagerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/github/avpcretois/boat_manager/BoatManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.avpcretois.boat_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
