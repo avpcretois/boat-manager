f5b6f0121c274cd309a65cfbf468dfa2d643ba8b
Andréas Crétois <avpcretois@use.startmail.com>
2025-12-12 11:17:55 +0100

    chore: Sets the angular naming convention to pre v20

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app';
+import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideRouter([])]
     }).compileComponents();
   });
 
